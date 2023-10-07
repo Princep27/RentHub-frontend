fe33900d7aad96c20586bde76a9dc559636aba0b
CaptainAwesomeRaj <raj6264186210@gmail.com>
2023-10-07 06:50:39 +0530

    filter feature changes
    added all the states and cities to filter feature

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,40 +6,52 @@ import Footer from '../../components/footer/Footer';
 import {BiFilter} from "react-icons/bi";
 import {ImCross} from "react-icons/im";
 import {FiSearch} from "react-icons/fi";
-import {states,cities} from "../../data.js";
 import { LoginContext } from '../../context/LoginContext';
 import Login from '../login/Login';
+import { Country, State, City }  from 'country-state-city';
+
 
 export default function Home() {
 
+  // States and contexts
   const [isOpen,setIsOpen] = useState(false);
   const [filterState,setFilterState] = useState([]);
   const [filterCities,setFilterCities] = useState([]);
-  const [selectedState,setSelectedState] = useState("");
-  const [selectedCity,setSelectedCity] = useState("");
+  const [selectedState,setSelectedState] = useState({name:"",selected:false});
+  const [selectedCity,setSelectedCity] = useState({name:"",selected:false});
   const {isLoginFormOpen} = useContext(LoginContext);
+
+  // Effetcs
+
+  useEffect(()=>{
+    setFilterState(
+      State.getStatesOfCountry('IN').map(
+        (val)=>{
+          return {name:val.name,code:val.isoCode}
+        }
+      )
+    );
+  },[])
+
+  useEffect(()=>{
+    selectedState.selected && setFilterCities(City.getCitiesOfState('IN',selectedState.code).map((val)=>{ return val.name}));
+  },[selectedState]);
+
+  // Event handlers
 
   function handleFilterButton(){
     setIsOpen(!isOpen);
-    setFilterState(states);
-    setFilterCities(cities.top);
   }
-
+  
   function handleInputStateChange(e){
-    let temp = states.filter(state => state.toLowerCase().startsWith(e.target.value.toLowerCase()));
-    setFilterState(temp);
-    setSelectedState(e.target.value);
+    setSelectedState({name:e.target.value,selected:false});
+    setSelectedCity({name:"",selected:false});
   }
 
   function handleInputCityChange(e){
-    let temp = cities[selectedState] && cities[selectedState].filter(city => city.toLowerCase().startsWith(e.target.value.toLowerCase()));
-    setFilterCities(temp);
-    setSelectedCity(e.target.value);
+    setSelectedCity({name:e.target.value,selected:false});
   }
 
-  useEffect(()=>{
-    setFilterCities(cities[selectedState]);
-  },[selectedState]);
 
   return (
     <>
@@ -65,23 +77,34 @@ export default function Home() {
 
 
                 <div className='filterState' >
-                   <input className='filterStateInput' value={selectedState} placeholder='Select State' onChange={handleInputStateChange}></input>
+                   <input className='filterStateInput' value={selectedState.name} placeholder='Select State' onChange={handleInputStateChange}></input>
                    <ul className='filterStateDropdown'>
                     {
-                      setFilterState && filterState.map((val,index)=>{
-                        return <li key={index} className='filterStateDropdownItem' onClick={()=>setSelectedState(val)} >{val}</li>
-                      })
+                      filterState
+                      .filter(
+                        (val)=>{
+                          return val.name.toLowerCase().startsWith(selectedState.name.toLowerCase());
+                        }
+                      )
+                      .map(
+                          (val,index)=>{
+                          return <li key={index} className='filterStateDropdownItem' onClick={()=>setSelectedState({...val,selected:true})} >{val.name}</li>
+                        }
+                      )
                     }
                    </ul>
                    <FiSearch className='filterStateInputSearch'/>
                 </div>
 
                 <div className='filterCity' >
-                    <input className='filterCityInput' value={selectedCity} placeholder='Select City' onChange={handleInputCityChange}></input>
+                    <input className='filterCityInput' value={selectedCity.name} placeholder='Select City' onChange={handleInputCityChange}></input>
                     <ul className='filterCityDropdown'>
                     {
-                      filterCities && filterCities.map((val,index)=>{
-                        return <li key={index} className='filterCityDropdownItem' onClick={()=>setSelectedCity(val)} >{val}</li>
+                      selectedState.selected &&
+                      filterCities
+                      .filter((val)=>val.toLowerCase().startsWith(selectedCity.name.toLowerCase()))
+                      .map((val,index)=>{
+                        return <li key={index} className='filterCityDropdownItem' onClick={()=>setSelectedCity({name:val,selected:true})} >{val}</li>
                       })
                     }
                     </ul>
