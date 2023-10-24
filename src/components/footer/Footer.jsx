80529748dcbadcdcbb824d8d7efe93811cb5e9ac
CaptainAwesomeRaj <raj6264186210@gmail.com>
2023-10-24 08:43:19 +0530

    footer refurbished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,53 +1,54 @@
+import { Col, Container, Row, Stack } from 'react-bootstrap';
 import './footer.css';
 import { AiFillFacebook,AiOutlineInstagram,AiOutlineTwitter,AiFillYoutube,AiFillLinkedin} from "react-icons/ai";
 export default function Footer(){
     return (
         <>  
             <footer >
-                    <div className="footerIconsWrapper">
+                <Container fluid className='p-4'>
+                    <Row>
+                    <Col xl={4} lg={5}>
                         <h2>RentHub</h2>
-                        <div className="footerIcons">
-                            <AiFillFacebook/>
-                            <AiOutlineInstagram/>
-                            <AiOutlineTwitter/>
-                            <AiFillYoutube/>
-                            <AiFillLinkedin/>
-                        </div>
-                <div className="copyright">	&#169;2023 RentHub. LLC. All Rights Reserved.</div>
-                <hr/>
-                    </div>
-                <div className="footerWrapper">
-                    <div className="footerLinks">
-                        <h4>Quick Links</h4>
-                        <ul>
-                            <li>Book</li>
-                            <li>Car</li>
-                            <li>Room</li>
-                            <li>Camera</li>
-                            <li>Home Appliances</li>
-                        </ul>
-                    </div>
-                    <div className="footerVerticalRule" id="footerVerticalRule1"></div>
-                    <div className="footerLinks">
-                        <h4>Account</h4>
-                        <ul>
-                            <li>Home</li>
-                            <li>Sign Up</li>
-                            <li>Login</li>
-                        </ul>
-                    </div>
+                        <Stack direction='horizontal' gap={4} className='mb-3'>
+                            <AiFillFacebook className='footer-icon'/>
+                            <AiOutlineInstagram className='footer-icon'/>
+                            <AiOutlineTwitter className='footer-icon'/>
+                            <AiFillYoutube className='footer-icon'/>
+                            <AiFillLinkedin className='footer-icon'/>
+                        </Stack>
+                        <div>&#169;2023 RentHub. LLC. All Rights Reserved.</div>
+                        <hr className='d-lg-block d-none' style={{maxWidth:'400px'}}/>
+                        <hr className='d-lg-none'/>
+                    </Col>
 
-                    <div className="footerVerticalRule" id='footerVerticalRule2'></div>
-                    <div className="footerLinks">
-                        <ul>
-                            <li>About</li>
-                            <li>Careers</li>
-                            <li>Terms of Use</li>
-                            <li>Privacy Policy</li>
-                        </ul>
-                    </div>
-                </div>
-                <hr/>
+                    <Col lg={7}>
+                        <Stack direction='horizontal' className='flex-wrap align-items-start justify-content-around'>
+                            <div>
+                                <h5>Quick Links</h5>
+                                <div>Book           </div>
+                                <div>Car            </div>
+                                <div>Room           </div>
+                                <div>Camera         </div>
+                                <div>Home Appliances</div>
+                            </div>
+                            <div className="vr"></div>
+                            <div>
+                                <h5>Account</h5>
+                                <div>Home   </div>
+                                <div>Sign Up</div>
+                                <div>Login  </div>
+                            </div>
+                            <div className="vr d-none d-sm-block"></div>
+                            <div className='d-none d-sm-block'>
+                                <div>About         </div>
+                                <div>Careers       </div>
+                                <div>Terms of Use  </div>
+                                <div>Privacy Policy</div>
+                            </div>
+                        </Stack>
+                    </Col>
+                    </Row>
+                </Container>
             </footer>
         </>
     )
